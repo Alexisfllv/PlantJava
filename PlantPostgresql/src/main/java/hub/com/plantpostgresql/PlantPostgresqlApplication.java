c5bf395f7214bc982ee681c029c1af9c569766af
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-08-25 16:06:48 -0500

    salve pg

---
+++ PlantPostgresql/src/main/java/hub/com/plantpostgresql/PlantPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package hub.com.plantpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantPostgresqlApplication.class, args);
+    }
+
+}
---
+++ PlantPostgresql/src/test/java/hub/com/plantpostgresql/PlantPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.com.plantpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
