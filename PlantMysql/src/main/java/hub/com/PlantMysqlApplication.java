65321febdebe8a7454c068372ebf755eece5ee02
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-08-25 15:22:35 -0500

    base

---
+++ PlantMysql/src/main/java/hub/com/PlantMysqlApplication.java
@@ -0,0 +1,13 @@
+package hub.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantMysqlApplication.class, args);
+    }
+
+}
---
+++ PlantMysql/src/test/java/hub/com/PlantMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
